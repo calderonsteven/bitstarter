4e2af6ec5fa0018c80e8494977c9ab65f89567d0
Steven Calderon <calderonsteven@gmail.com>
2013-07-11 03:45:52 +0000

    using readFileSync and buffer.toString

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var bufferIndex = fs.readFileSync('index.html');
+  var text = bufferIndex.toString();
+
+  Response.send(text);
 });
 
 var port = process.env.PORT || 5000;
